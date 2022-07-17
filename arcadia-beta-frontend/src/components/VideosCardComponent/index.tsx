053ba32b1e29f7e74dde986d805f534afd6683f2
rafael-projects <68446421+rafael-projects@users.noreply.github.com>
2022-07-17 01:07:54 -0300

    securityConfig -> arcadia-project

--- arcadia-beta-frontend/src/components/VideosCardComponent/index.tsx
+++ arcadia-beta-frontend/src/components/VideosCardComponent/index.tsx
@@ -1,136 +1,147 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import NotificationButton from "../NotificationButton";
 import './styles.css'
 
+
+
 function VideosCardComponent() {
-return(
-<div className="videos-card">
-                    <h2 className="videos-titles">Videos</h2>
-                    <div>
-                        <div className="video-form-container">
-                            <input className="video-form"type="text" />
-                        </div>
-                        <div className="video-form-container">
-                            <input className="video-form" type="text" />
-                        </div>
-                    </div>
-                
-                <div>
-                    <table className="table-classification">
-                        <thead>
-                            <tr>
-                                <th className="show992">ID</th>
-                                <th className="show992">Data Postagem</th>
-                                <th>Jogadores</th>
-                                <th className="show992">Pontuação</th>
-                                <th className="show992">Ranking</th>
-                                <th>Total</th>
-                                <th>Notificar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">13/07/2022</td>
-                                <td>RafaelZeroWins</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>55300-pts</td>
-                                <td>
-                                    <div className="red-btn-container">
-                                        <div className="red-btn">
-                                            <img src="./Vector.svg" alt="image-icon" />
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                            
-                            
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">13/07/2022</td>
-                                <td>RafaelZeroWins</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>55300-pts</td>
-                                <td>
-                                    <div className="red-btn-container">
-                                        <div className="red-btn">
-                                            <img src="./Vector.svg" alt="image-icon"/>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
 
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">13/07/2022</td>
-                                <td>RafaelZeroWins</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>55300-pts</td>
-                                <td>
-                                    <div className="red-btn-container">
-                                        <div className="red-btn">
-                                            <img src="./Vector.svg" alt="image-icon" />
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
+    const max = new Date();
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
 
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">13/07/2022</td>
-                                <td>RafaelZeroWins</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>55300-pts</td>
-                                <td>
-                                    <div className="red-btn-container">
-                                        <div className="red-btn">
-                                            <img src="./Vector.svg" alt="image-icon" />
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                            
-                            
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">13/07/2022</td>
-                                <td>RafaelZeroWins</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>55300-pts</td>
-                                <td>
-                                    <div className="red-btn-container">
-                                        <div className="red-btn">
-                                            <img src="./Vector.svg" alt="image-icon" />
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
+    const  [maxDate, setMaxDate] = useState(max);
+    const  [minDate, setMinDate] = useState(min);
 
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">13/07/2022</td>
-                                <td>RafaelZeroWins</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>55300-pts</td>
-                                <td>
-                                    <div className="red-btn-container">
-                                        <div className="red-btn">
-                                            <img src="./Vector.svg" alt="image-icon" />
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-
-                    </table>
+    return (
+        <div className="videos-card">
+            <h2 className="videos-titles">Videos</h2>
+            <div>
+                <div className="video-form-container">
+                    <DatePicker
+                        selected={minDate}
+                        onChange={(date: Date) => setMinDate(date)}
+                        className="video-form"
+                        dateFormat="dd/MM/yyyy"
+                    />
+                </div>
+                <div className="video-form-container">
+                    <DatePicker
+                        selected={maxDate}
+                        onChange={(date: Date) => setMaxDate(date)}
+                        className="video-form"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
             </div>
 
-)
+            <div>
+                <table className="table-classification">
+                    <thead>
+                        <tr>
+                            <th className="show992">ID</th>
+                            <th className="show992">Data Postagem</th>
+                            <th>Jogadores</th>
+                            <th className="show992">Pontuação</th>
+                            <th className="show992">Ranking</th>
+                            <th>Total</th>
+                            <th>Notificar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">13/07/2022</td>
+                            <td>RafaelZeroWins</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>55300-pts</td>
+                            <td>
+                                <div className="red-btn-container">
+                                    <NotificationButton/>
+                                </div>
+                            </td>
+                        </tr>
+
+
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">13/07/2022</td>
+                            <td>RafaelZeroWins</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>55300-pts</td>
+                            <td>
+                                <div className="red-btn-container">
+                                <NotificationButton/>
+                                </div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">13/07/2022</td>
+                            <td>RafaelZeroWins</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>55300-pts</td>
+                            <td>
+                                <div className="red-btn-container">
+                                <NotificationButton/>
+                                </div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">13/07/2022</td>
+                            <td>RafaelZeroWins</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>55300-pts</td>
+                            <td>
+                                <div className="red-btn-container">
+                                <NotificationButton/>
+                                </div>
+                            </td>
+                        </tr>
+
+
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">13/07/2022</td>
+                            <td>RafaelZeroWins</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>55300-pts</td>
+                            <td>
+                                <div className="red-btn-container">
+                                <NotificationButton/>
+                                </div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">13/07/2022</td>
+                            <td>RafaelZeroWins</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>55300-pts</td>
+                            <td>
+                                <div className="red-btn-container">
+                                <NotificationButton/>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+
+    )
 }
 
 export default VideosCardComponent
